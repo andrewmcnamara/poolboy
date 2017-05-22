f10db31b9413c3d03d0e4bf30ebf69452ad9d394
Andrew McNamara <andrew.mcnamara@gmail.com>
2017-05-22 22:07:07 +1000

    Roughing out object structure

---
+++ index.js
@@ -0,0 +1,16 @@
+class PoolBoy {
+  constructor(maxObjects){
+   this.maxObjects = maxObjects;
+   this.objectQueue = [];
+  }
+
+
+  function getObject(){
+   if (this.objectQueue.length ==0){
+    
+   }
+     this.objectQueue.pop(); 
+    
+ }
+
+}
